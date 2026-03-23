79e7e9d5d86c284a334d6cf97bc8c42ebada7786
zwirok-git <oleksijprokopenko@gmail.com>
2026-03-23 18:54:17 +0200

    implemented cache

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        if args in cache_dict.keys():
+            print("Getting from cache")
+            return cache_dict[args]
+
+        else:
+            cache_dict.update({args: func(*args, **kwargs)})
+            print("Calculating new result")
+            return cache_dict[args]
+
+    return wrapper
